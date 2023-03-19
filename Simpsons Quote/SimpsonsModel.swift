925032cc29fa90ac1be4a74f1d6759aa1f350837
TypeCc <serif.mete.aksoy@gmail.com>
2023-03-19 05:15:25 -0700

    Initial Commit

---
+++ Simpsons Quote/SimpsonsModel.swift
@@ -0,0 +1,14 @@
+//
+//  SimpsonsModel.swift
+//  Simpsons Quote
+//
+//  Created by serif mete on 19.03.2023.
+//
+
+import Foundation
+// MARK: - Quote
+struct Quote: Codable {
+    let quote: String?
+    let image: String?
+    let character: String?
+}
---
+++ Simpsons Quote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simpsons Quote
+//
+//  Created by serif mete on 19.03.2023.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
